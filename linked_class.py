2922da86b455e11f141443cf7de735eb3d4481f0
Esenova <47048618+Esenova@users.noreply.github.com>
2019-03-04 13:43:47 +0600

    Update linked_class.py

--- linked_class.py
+++ linked_class.py
@@ -1,25 +1,34 @@
-class Node:
-    def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+from linked_list import MyList, ListNode
+
 class Stack:
-    def __init__(self):
-        self.head = None
- 
-    def push(self, data):
-        if self.head is None:
-            self.head = Node(data)
+	def __init__(self):
+		self.my_stack = MyList()
+        
+    def empty(self):
+		if self.length() == 0:
+			return True
+		return False
+		
+	def length(self):
+		return self.my_stack.__len__()
+    
+    def peek(self):
+		if self.empty():
+            print("Stack is empty")
+		    return None
         else:
-            new_node = Node(data)
-            new_node.next = self.head
-            self.head = new_node
- 
-    def pop(self):
-        if self.head is None:
+			return self.my_stack.head.get_data()
+		
+	def pop(self):
+		if self.empty():
+			print("Stack is empty")
             return None
         else:
-            popped = self.head.data
-            self.head = self.head.next
-            return popped
- 
+            obj_data = self.my_stack.head.get_data()
+			self.my_stack.remove(self.my_stack.head.get_data())
+			return obj_data
+			
+		
+	def push(self, obj):
+		obj =  ListNode(obj)
+		self.my_stack.add(obj)
